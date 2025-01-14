fe58b3a666e863945e13ced54077d31624565dae
Артур <marsel839@gmail.com>
2025-01-14 22:55:13 +0300

    Fixed .gitignore and removed unnecessary files from tracking

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Project, Task, TaskFile, ProjectMember, Comment, TaskHistory
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(TaskFile)
+admin.site.register(ProjectMember)
+admin.site.register(Comment)
+admin.site.register(TaskHistory)
